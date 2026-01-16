323ca5faec3896908614283bea4cc065a7cfa5e5
TechTonic <amoohalleluyah1@gmail.com>
2026-01-16 17:45:28 +0100

    change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.random.*;;
+
 
 // public class Main {
 //     public static void main(String[] args) {
@@ -65,13 +65,29 @@ import java.util.random.*;;
 //     }
 // }
 
+
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
         Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 0-50:");
+        int inputNum = input.nextInt();
+        int randomNum = rand.nextInt(50);
 
-        int randomNumber = rand.nextInt((50));
-
-        System.out.println(randomNumber);
+        int i = 4;
+        while (i > 0) {
+            System.out.println("You have " + i + " attempts left.");
+            if (inputNum == randomNum) {
+                System.out.println("You guessed the correct number!");
+                break;
+            } else {
+                System.out.println("Wrong guess. Try again:");
+                inputNum = input.nextInt();
+            }
+            i--;
+            System.out.println("Aw SHoot! out of tries!");
+            System.out.println("The correct number was: " + randomNum);
+        }
+        input.close();
     }
 }
